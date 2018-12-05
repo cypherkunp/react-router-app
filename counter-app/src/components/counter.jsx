59fb6c756e1e14341ec7ef29b87d1b89abaac305
Devvrat Shukla <dshukla@ptc.com>
2018-12-05 21:49:52 +0530

    tutorial: 8 - Rendering Lists

---
+++ counter-app/src/components/counter.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondry btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 ";
+    classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
