384475bb1b589f251e4c48ead80111cb2bfd412d
Devvrat Shukla <dshukla@ptc.com>
2018-12-05 23:55:35 +0530

    tut: 12 - Updating the state

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -17,8 +17,8 @@ class Counter extends Component {
   }
 
   handleIncrement = () => {
-    console.log("Increment is clicked", this);
-  }
+    this.setState({ count: this.state.count + 1 });
+  };
 
   render() {
     return (
