f39326228515794573f21370ae20b3a600830c3b
Devvrat Shukla <dshukla@ptc.com>
2018-12-10 14:42:40 +0530

    tut: 3- Passing data to components and 4- Passing Children

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,32 +2,35 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    value: this.props.value
   };
 
   handleIncrement = params => {
     console.log(params);
     // tells react that the state of the component has changes and react will call the render
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   getBadgeClasses() {
     let classes = "badge m-2 ";
-    classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+    classes += this.state.value === 0 ? "badge-warning" : "badge-primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 
   render() {
+    console.log("Props set > ", this.props);
+
     return (
       <div>
+        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => {this.handleIncrement({id:1})}}
+          onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
