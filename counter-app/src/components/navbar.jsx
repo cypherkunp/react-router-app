a8cf3d022ef6e2c3611a4f4a48c1729e4ed74c0a
Devvrat Shukla <dshukla@ptc.com>
2018-12-11 23:39:02 +0530

    tut: 14-Argument destructuring

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 
 // Stateless functional component
-const Navbar = props => {
+const Navbar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-dark bg-dark">
       <a className="navbar-brand" href="#">
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
