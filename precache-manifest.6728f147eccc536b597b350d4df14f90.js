5e231035c84ae979e49a63793d7a65047eaf8e45
Devvrat Shukla <dshukla@ptc.com>
2019-05-26 15:32:46 +0530

    Updates

---
+++ precache-manifest.6728f147eccc536b597b350d4df14f90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66ff8add5681644b624aaab7825860ef",
+    "url": "/react-router-app/index.html"
+  },
+  {
+    "revision": "d0fdba887f39c193530a",
+    "url": "/react-router-app/static/css/main.e829653b.chunk.css"
+  },
+  {
+    "revision": "8ab691917232d5427464",
+    "url": "/react-router-app/static/js/2.68ddf8c0.chunk.js"
+  },
+  {
+    "revision": "d0fdba887f39c193530a",
+    "url": "/react-router-app/static/js/main.ebcb3280.chunk.js"
+  },
+  {
+    "revision": "80e81a85950596580de4",
+    "url": "/react-router-app/static/js/runtime~main.1f157a88.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cead8e8d7c94c804d382880d4ae76bcc.js"
+  "/react-router-app/precache-manifest.6728f147eccc536b597b350d4df14f90.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-router-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
